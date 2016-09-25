b34c949d51598a1ab222ccf6fb9e12bbb92890cd
Alistair Green <alistair@glidingweb.org>
2016-09-25 17:00:56 +0100

    Add links to home page.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Button, ButtonToolbar, Jumbotron } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const HomePage = () => {
   return (
-    <div>
+    <Jumbotron>
       <h1>Soaring Analyst</h1>
 
-      <p>Lorem ipsum dolor sit amet...</p>
-    </div>
+      <p>
+        A free browser-based flight planning and analysis tool for glider pilots.
+      </p>
+
+      <p>
+        &copy; 2016 Alistair Green
+      </p>
+
+      <ButtonToolbar>
+        <LinkContainer to="/task">
+          <Button bsStyle="primary">Plan a cross country task </Button>
+        </LinkContainer>
+        <LinkContainer to="/igcview">
+          <Button bsStyle="success">View an IGC logger trace </Button>
+        </LinkContainer>
+      </ButtonToolbar>
+    </Jumbotron>
   );
 };
 
