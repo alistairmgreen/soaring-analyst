34bb07b0e6c5c31906c875e84760f8f835cf8a46
Alistair Green <alistair@glidingweb.org>
2016-10-13 20:00:01 +0100

    Change assertion style to 'should'.

--- src/parser/parseHeaders.spec.js
+++ src/parser/parseHeaders.spec.js
@@ -1,7 +1,9 @@
-import {expect} from 'chai';
+import chai from 'chai';
 import MockDate from 'mockdate';
 import moment from 'moment';
 import parseHeaders from './parseHeaders';
+
+chai.should();
 
 describe('parseHeaders function', function() {
   const fakeNow = moment.utc([2015, 9, 10, 23, 0, 0]);
@@ -22,27 +24,27 @@ describe('parseHeaders function', function() {
     });
 
     it('returns a header named "Date"', function() {
-      expect(result.name).to.equal("Date");
+      result.name.should.equal("Date");
     });
 
     it('returns a valid moment object', function() {
-      expect(result.value.isValid()).to.be.true;
+      result.value.isValid().should.be.true;
     });
 
     it('returns a moment in UTC mode', function() {
-      expect(result.value.creationData().isUTC).to.be.true;
+      result.value.creationData().isUTC.should.be.true;
     });
 
     it('returns a year in the 21st century', function() {
-      expect(result.value.year()).to.equal(2014);
+      result.value.year().should.equal(2014);
     });
 
     it('returns the correct day of the month', function() {
-      expect(result.value.date()).to.equal(28);
+      result.value.date().should.equal(28);
     });
 
     it('returns the correct zero-indexed month', function() {
-      expect(result.value.month()).to.equal(3);
+      result.value.month().should.equal(3);
     });
   });
 
@@ -54,27 +56,27 @@ describe('parseHeaders function', function() {
     });
 
     it('returns a header named "Date"', function() {
-      expect(result.name).to.equal("Date");
+      result.name.should.equal("Date");
     });
 
     it('returns a valid moment object', function() {
-      expect(result.value.isValid()).to.be.true;
+      result.value.isValid().should.be.true;
     });
 
     it('returns a moment in UTC mode', function() {
-      expect(result.value.creationData().isUTC).to.be.true;
+      result.value.creationData().isUTC.should.be.true;
     });
 
     it('returns a year in the 21st century', function() {
-      expect(result.value.year()).to.equal(2015);
+      result.value.year().should.equal(2015);
     });
 
     it('returns the correct day of the month', function() {
-      expect(result.value.date()).to.equal(28);
+      result.value.date().should.equal(28);
     });
 
     it('returns the correct zero-indexed month', function() {
-      expect(result.value.month()).to.equal(3);
+      result.value.month().should.equal(3);
     });
   });
 
@@ -86,27 +88,27 @@ describe('parseHeaders function', function() {
     });
 
     it('returns a header named "Date"', function() {
-      expect(result.name).to.equal("Date");
+      result.name.should.equal("Date");
     });
 
     it('returns a valid moment object', function() {
-      expect(result.value.isValid()).to.be.true;
+      result.value.isValid().should.be.true;
     });
 
     it('returns a moment in UTC mode', function() {
-      expect(result.value.creationData().isUTC).to.be.true;
+      result.value.creationData().isUTC.should.be.true;
     });
 
     it('returns a year in the 20th century', function() {
-      expect(result.value.year()).to.equal(1995);
+      result.value.year().should.equal(1995);
     });
 
     it('returns the correct day of the month', function() {
-      expect(result.value.date()).to.equal(28);
+      result.value.date().should.equal(28);
     });
 
     it('returns the correct zero-indexed month', function() {
-      expect(result.value.month()).to.equal(3);
+      result.value.month().should.equal(3);
     });
   });
 
