81e53bc586c9832fd06aea6acb512db1d1af95c7
Alistair Green <alistair@glidingweb.org>
2016-10-15 00:02:36 +0100

    Parse latitude / longitude in IGC format.

---
+++ src/parser/parseLatitudeLongitude.js
@@ -0,0 +1,44 @@
+const DEGREES_SYMBOL = '\u00B0';
+const MINUTES_SYMBOL = "'";
+
+function parseLatitude(latitude) {
+   return {
+    degrees: parseInt(latitude.substring(0, 2), 10),
+    minutes: parseInt(latitude.substring(2, 7), 10) / 1000.0,
+    direction: latitude[latitude.length - 1]
+  };
+}
+
+function parseLongitude(longitude) {
+  return {
+    degrees: parseInt(longitude.substring(0, 3), 10),
+    minutes: parseInt(longitude.substring(3, 8), 10) / 1000.0,
+    direction: longitude[longitude.length - 1]
+  };
+}
+
+function toDecimalDegrees(degreesAndMinutes) {
+  let direction = degreesAndMinutes.direction.toUpperCase();
+  let sign = (direction === 'N' || direction === 'E') ? 1 : -1;
+
+  return sign * (degreesAndMinutes.degrees + degreesAndMinutes.minutes / 60.0);
+}
+
+function formatDegreesMinutes(degMin) {
+  let deg = degMin.degrees.toFixed(0);
+  let min = degMin.minutes.toFixed(3);
+  let direction = degMin.direction;
+
+  return deg + DEGREES_SYMBOL + min + MINUTES_SYMBOL + direction;
+}
+
+export default function parseLatitudeLongitude(latLonString) {
+  let latitude = parseLatitude(latLonString.substring(0, 8));
+  let longitude = parseLongitude(latLonString.substring(8));
+
+  return {
+    lat: toDecimalDegrees(latitude),
+    lon: toDecimalDegrees(longitude),
+    toString: () => formatDegreesMinutes(latitude) + ", " + formatDegreesMinutes(longitude)
+  };
+}
---
+++ src/parser/parseLatitudeLongitude.spec.js
@@ -0,0 +1,47 @@
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import parseLatitudeLongitude from './parseLatitudeLongitude';
+
+chai.should();
+
+describe('parseLatitudeLongitude function', function () {
+  describe('Given a northern latitude and eastern longitude', function () {
+    let result;
+
+    beforeEach(function () {
+      result = parseLatitudeLongitude("5215195N00043254E");
+    });
+
+    it('returns positive latitude as decimal degrees', function() {
+      result.lat.should.be.approximately(52.253250, 1.0e-7);
+    });
+
+    it('returns positive longitude as decimal degrees', function() {
+      result.lon.should.be.approximately(0.720900, 1.0e-7);
+    });
+
+    it('gives a formatted string', function () {
+      result.toString().should.equal("52°15.195'N, 0°43.254'E");
+    });
+  });
+
+  describe('Given a southern latitude and western longitude', function () {
+    let result;
+
+    beforeEach(function () {
+      result = parseLatitudeLongitude("5215195S00043254W");
+    });
+
+    it('returns negative latitude as decimal degrees', function() {
+      result.lat.should.be.approximately(-52.253250, 1.0e-7);
+    });
+
+    it('returns negative longitude as decimal degrees', function() {
+      result.lon.should.be.approximately(-0.720900, 1.0e-7);
+    });
+
+    it('gives a formatted string', function () {
+      result.toString().should.equal("52°15.195'S, 0°43.254'W");
+    });
+  });
+});
