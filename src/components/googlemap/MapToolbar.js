fc1e46e678e155a5c0e95026fe50a8952665fc59
Alistair Green <alistair@glidingweb.org>
2016-10-30 01:03:01 +0100

    Add icon to map zoom menu.

--- src/components/googlemap/MapToolbar.js
+++ src/components/googlemap/MapToolbar.js
@@ -1,39 +1,44 @@
 import React, { PropTypes } from 'react';
-import { ButtonToolbar, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
+import { ButtonToolbar, ButtonGroup, Dropdown, Glyphicon, MenuItem } from 'react-bootstrap';
 import { List } from 'immutable';
 
-function MapToolbar(props){
+function MapToolbar(props) {
   let waypointItems = [];
   const waypointNames = props.waypointNames;
   if (waypointNames) {
     let lastIndex = waypointNames.count() - 1;
     waypointItems = waypointNames.map((name, index) => {
-    let label;
+      let label;
 
-    if (index === lastIndex){
-      label = 'Finish:';
-    }
-    else if (index === 0) {
-      label = 'Start:';
-    }
-    else {
-      label = `Turnpoint ${index}:`;
-    }
-    return (
-      <MenuItem key={index+2} onClick={()=>props.zoomToWaypoint(index)}>
-      <strong>{label}</strong> {name}
-      </MenuItem>);
-  });
+      if (index === lastIndex) {
+        label = 'Finish:';
+      }
+      else if (index === 0) {
+        label = 'Start:';
+      }
+      else {
+        label = `Turnpoint ${index}:`;
+      }
+      return (
+        <MenuItem key={index + 2} onClick={() => props.zoomToWaypoint(index)}>
+          <strong>{label}</strong> {name}
+        </MenuItem>);
+    });
   }
 
   return (
     <ButtonToolbar>
       <ButtonGroup bsSize="large">
-        <DropdownButton title="Zoom to...">
-          <MenuItem onClick={props.zoomToFlightPath}><strong>Flight path</strong></MenuItem>
-          <MenuItem divider />
-          {waypointItems}
-        </DropdownButton>
+        <Dropdown>
+          <Dropdown.Toggle>
+            <Glyphicon bsClass="fa" glyph="search" /> &nbsp; Zoom to...
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <MenuItem onClick={props.zoomToFlightPath}><strong>Flight path</strong></MenuItem>
+            <MenuItem divider />
+            {waypointItems}
+          </Dropdown.Menu>
+        </Dropdown>
       </ButtonGroup>
     </ButtonToolbar>
   );
