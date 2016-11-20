74549bda259479fdcbb1fdc1aa887cff450b1a94
Alistair Green <alistair@glidingweb.org>
2016-11-20 18:51:46 +0000

    Set webpack public path for production.
    Needed to make it work on GitHub pages. However it breaks the automatic preview.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/soaring-analyst',
     filename: '[name].[chunkhash].js'
   },
   externals: {
