a365fdee4ade7629174138fec162475fdfbb3cd1
Alistair Green <alistair@glidingweb.org>
2016-11-20 16:49:50 +0000

    Improve startup banner. Replaces homepage.

--- src/components/loggertrace/StartupBanner.js
+++ src/components/loggertrace/StartupBanner.js
@@ -1,30 +1,55 @@
 import React, { PropTypes } from 'react';
-import { Alert, Jumbotron, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap';
+import { Alert, Button, Jumbotron, FormGroup, ControlLabel, HelpBlock, Grid, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import IGCFilePicker from '../IGCFilePicker';
 
 function StartupBanner(props) {
   return (
     <Jumbotron>
-      <h1>View a Logger Trace</h1>
+      <div style={{ textAlign: "center" }}>
+        <h1>Soaring Analyst</h1>
 
-      <form>
-        <FormGroup controlId="igcFileSelector" bsSize="lg">
-          <ControlLabel>
-            Choose an IGC file to view:
+        <p>
+          A free browser-based flight planning and analysis tool for glider pilots.
+      </p>
+
+        <p>
+          &copy; 2016 Alistair Green
+      </p>
+      </div>
+
+      <Grid>
+        <Row>
+          <Col sm={12} md={6}>
+            <h2> Logger trace viewer </h2>
+            <form>
+              <FormGroup controlId="igcFileSelector" bsSize="lg">
+                <ControlLabel>
+                  Choose an IGC logger trace file to view:
           </ControlLabel>
-          <IGCFilePicker onChooseFile={props.loadFile} />
-          <HelpBlock>
-            This file will be opened inside your browser; it won't
+                <IGCFilePicker onChooseFile={props.loadFile} />
+                <HelpBlock>
+                  This file will be opened inside your browser; it won't
             be uploaded to the Internet.
           </HelpBlock>
-        </FormGroup>
-      </form>
+              </FormGroup>
+            </form>
 
-      {props.errorMessage && (
-        <Alert bsStyle="danger">
-          <strong>{props.errorMessage}</strong>
-        </Alert>
-      ) }
+            {props.errorMessage && (
+              <Alert bsStyle="danger">
+                <strong>{props.errorMessage}</strong>
+              </Alert>
+            )}
+          </Col>
+          <Col sm={12} md={6}>
+            <h2> Task planner </h2>
+
+            <LinkContainer to="/task">
+              <Button bsStyle="primary">Plan a cross country task </Button>
+            </LinkContainer>
+          </Col>
+        </Row>
+      </Grid>
     </Jumbotron>
   );
 }
