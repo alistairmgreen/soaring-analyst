fd12f4d2b05cecf46acbafc6449af96711582754
Alistair Green <alistair@glidingweb.org>
2016-12-22 21:28:19 +0000

    Additional unit tests for altitude selectors.
    Includes test for getAltitudeUnitAbbreviation.

--- src/selectors/altitudeSelectors.spec.js
+++ src/selectors/altitudeSelectors.spec.js
@@ -7,42 +7,16 @@ import * as selectors from './altitudeSelectors';
 
 chai.should();
 
-describe('getAltitudeUnit', function () {
-  const state = {
-    altitude: {
-      unit: FEET
-    }
-  };
-
-  it('returns the unit name', function () {
-    selectors.getAltitudeUnit(state)
-      .should.equal(FEET);
-  });
-});
-
-describe('getAltitudeSource', function () {
-  const state = {
-    altitude: {
-      source: GPS
-    }
-  };
-
-  it('returns the altitude source', function () {
-    selectors.getAltitudeSource(state)
-      .should.equal(GPS);
-  });
-});
-
 const GPS_METRES = List.of(1, 2, 3);
 const GPS_FEET = GPS_METRES.map(x => x * selectors.M_TO_FT);
 const PRESSURE_METRES = List.of(4, 5, 6);
 const PRESSURE_FEET = PRESSURE_METRES.map(x => x * selectors.M_TO_FT);
 
 const testCases = [
-  { unit: METRES, source: GPS, expected: GPS_METRES, timeIndex: 0 },
-  { unit: FEET, source: GPS, expected: GPS_FEET, timeIndex: 1 },
-  { unit: METRES, source: PRESSURE, expected: PRESSURE_METRES, timeIndex: 2 },
-  { unit: FEET, source: PRESSURE, expected: PRESSURE_FEET, timeIndex: 1 }
+  { unit: METRES, abbreviation: 'm', source: GPS, expected: GPS_METRES, timeIndex: 0 },
+  { unit: FEET, abbreviation: 'ft', source: GPS, expected: GPS_FEET, timeIndex: 1 },
+  { unit: METRES, abbreviation: 'm', source: PRESSURE, expected: PRESSURE_METRES, timeIndex: 2 },
+  { unit: FEET, abbreviation: 'ft', source: PRESSURE, expected: PRESSURE_FEET, timeIndex: 1 }
 ];
 
 testCases.forEach(test => {
@@ -50,6 +24,10 @@ testCases.forEach(test => {
     const state = {
       altitude: {
         unit: test.unit,
+        unitAbbreviations: Map({
+          Feet: 'ft',
+          Metres: 'm'
+        }),
         source: test.source,
         values: Map({
           GPS: GPS_METRES,
@@ -58,6 +36,27 @@ testCases.forEach(test => {
       },
       timeIndex: test.timeIndex
     };
+
+    describe('getAltitudeUnit', function () {
+      it('returns the unit name', function () {
+        selectors.getAltitudeUnit(state)
+          .should.equal(test.unit);
+      });
+    });
+
+    describe('getAltitudeUnitAbbreviation', function () {
+      it(`returns the abbreviation "${test.abbreviation}"`, function () {
+        selectors.getAltitudeUnitAbbreviation(state)
+          .should.equal(test.abbreviation);
+      });
+    });
+
+    describe('getAltitudeSource', function () {
+      it('returns the altitude source', function () {
+        selectors.getAltitudeSource(state)
+          .should.equal(test.source);
+      });
+    });
 
     describe('getAltitudes', function () {
       it(`returns a list of ${test.source} altitudes in ${test.unit}`, function () {
